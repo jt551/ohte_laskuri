1147d0794fbf95388f89a2198276c1ea2bd500d2
jt551 <jtwebapps@gmail.com>
2020-10-09 12:14:03 +0300

    commit 3.1 end of exercise

--- src/laskuri2/Laskuri2.java
+++ src/laskuri2/Laskuri2.java
@@ -9,36 +9,47 @@ package laskuri2;
  *
  * @author juhat
  */
-public class Laskuri2{
-	// muokattu GitHubissa
-  /* laskuriolion kentan arvoa voi kasvattaa yhdella kutsumalla metodia
+public class Laskuri2 {
+    // muokattu GitHubissa
+
+    /* laskuriolion kentan arvoa voi kasvattaa yhdella kutsumalla metodia
      lisaa(). Arvoa voi tarkastella ja sen voi nollata.                 */
-	private int tapahtumia;
-        public Laskuri2(int alkuarvo){ 
+    public static void main(String[] args) {
+        Laskuri2 laskurini = new Laskuri2(5);
+        System.out.println(laskurini);
+    }
+    private int tapahtumia;
+
+    public Laskuri2(int alkuarvo) {
         // konstruktori, kuormitettu. Alkuarvon tulee olla >= 0.
-		if(alkuarvo < 0)
-			return;
-		this.tapahtumia = alkuarvo;
-	}
-	public Laskuri2(){
-		this.tapahtumia = 0;
-	}
-	
-  	public void lisaa(){
-    		this.tapahtumia = this.tapahtumia + 1; 
-  	}
-        
-        public void lisaa(int lisays){
-    		this.tapahtumia = this.tapahtumia + lisays; 
-  	}        
-  	public int lukema(){
-    		return this.tapahtumia;
-  	}
-  	public void nollaa(){
-    		this.tapahtumia = 0;
-  	}
-	public String toString(){
-		return "Lukema: " + this.tapahtumia;
-	}
+        if (alkuarvo < 0) {
+            return;
+        }
+        this.tapahtumia = alkuarvo;
+    }
+
+    public Laskuri2() {
+        this.tapahtumia = 0;
+    }
+
+    public void lisaa() {
+        this.tapahtumia = this.tapahtumia + 1;
+    }
+
+    public void lisaa(int lisays) {
+        this.tapahtumia = this.tapahtumia + lisays;
+    }
+
+    public int lukema() {
+        return this.tapahtumia;
+    }
+
+    public void nollaa() {
+        this.tapahtumia = 0;
+    }
+
+    public String toString() {
+        return "Lukema: " + this.tapahtumia;
+    }
 
 }
